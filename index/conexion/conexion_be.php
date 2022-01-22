b684151e7c383f048b1bd9eff73087929185a706
José López <96513678+MadBrain4@users.noreply.github.com>
2022-01-22 15:14:06 -0400

    Create conexion_be.php

---
+++ index/conexion/conexion_be.php
@@ -0,0 +1,15 @@
+<?php
+	$db_host = "localhost";
+	$db_usuario = "root";
+	$db_contra = "";
+	$db_nombre = "login_register_db";
+
+	$conexion = mysqli_connect($db_host,$db_usuario,$db_contra,$db_nombre);
+
+	/*if($conexion){
+		echo("Conectado exitosamente a la base de datos");
+	}
+	else{
+		echo("No se ha conectado a la base de datos");
+	}*/
+?>
