6f553090a7892f952c2a0206967e488f43f43cb2
José López <96513678+MadBrain4@users.noreply.github.com>
2022-01-22 15:27:11 -0400

    Update login_usuario_be.php

--- index/registro/login_usuario_be.php
+++ index/registro/login_usuario_be.php
@@ -1,23 +1,26 @@
 <?php
 
-	include('conexion_be.php');	
-	
+	include('../conexion/conexion_be.php');
+	//verifica si no se abierto seccion
 	if(!isset($_SESSION['usuario'])){
 		session_start();
+		//se declaran las variables
 		$correo = $_POST['correo'];
 		$contrasena = $_POST['contrasena'];
-		
-		$validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo = '$correo' and contrasena = '$contrasena'");
-		
+		//Se valida si hay un usuario con ese correo y contrasena
+		$validar_login = mysqli_query($conexion, "SELECT nombre_completo FROM usuarios WHERE correo = '$correo' and contrasena = '$contrasena'");
+		$files_login = mysqli_fetch_array($validar_login);
+		//en caso de que si, se envia a la pagina index_usuario
 		if(mysqli_num_rows($validar_login) > 0){
-			$_SESSION['usuario'] = $correo;
+			$_SESSION['correo'] = $correo;
+			$_SESSION['usuario'] = $files_login['nombre_completo'];
 			header("location: ../index_usuario.php ");
 			exit();
-		}
-		else{
+		//en caso de que no, se invia a la pagina registro
+		}else{
 			echo '
 				<script>
-					alert("Usuario no existe, por favor intentelo de nuevo");
+					alert("Usuario o contraseña incorrecta, por favor intentelo de nuevo");
 					window.location = "registro.php";
 				</script>
 			';
@@ -25,12 +28,9 @@
 		}
 		mysqli_close($conexion);
 	}
-	
-	if(isset($_SESSION['usuario'])){
-		echo
-			$_SESSION['usuario']
-		;
-	}
-	
 
+	if(isset($_SESSION['usuario'])){
+		echo $_SESSION['usuario'];
+	}
 ?>
+
